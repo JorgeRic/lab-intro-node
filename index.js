3124b9b014e0140bbcf84e729f072588232ea5e3
Jorge <jorrillop@hotmail.com>
2019-10-07 19:16:39 +0200

    Done

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor (items, length) {
+    this.items = []
+    this.length = 0
+  }
+
+  add (item) {
+    this.items[this.length] = item
+    this.items.sort((a, b) => (a - b))
+    this.length++
+  }
+
+  get (pos) {
+    return this.items[pos - 1]
+  }
+
+  max () {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.sort((a, b) => (b - a))
+      return this.items[0]
+    }
+  }
+
+  min () {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.sort((a, b) => (a - b))
+      return this.items[0]
+    }
+  }
+
+  sum () {
+    if (this.length === 0) {
+      return 0
+    } else {
+      this.items.reduce(function (a, b) {
+        return a + b
+      })
+    }
+  }
+
+  average () {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.reduce(function (a, b) {
+        return a + b / this.length
+      })
+    }
+  }
 };
 
-module.exports = SortedList;
+module.exports = SortedList
